ff68dc3a3eba260b940987b11ebb3857bd828419
daniafk <81378321+daniafk@users.noreply.github.com>
2021-04-05 18:55:33 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+def hello_gcs(event, context):
+
+    from google.cloud import bigquery
+
+    # Construct a BigQuery client object.
+    client = bigquery.Client()
+
+    # TODO(developer): Set table_id to the ID of the table to create.
+    table_id = "dev-country-196401.data_models.localidade"
+
+    job_config = bigquery.LoadJobConfig(
+        schema=[
+            bigquery.SchemaField("geolocation_zip_code_prefix", "INTEGER"),
+            bigquery.SchemaField("geolocation_lat", "FLOAT"),
+            bigquery.SchemaField("geolocation_lng", "FLOAT"),
+            bigquery.SchemaField("geolocation_city", "STRING"),
+            bigquery.SchemaField("geolocation_state", "STRING"),
+        ],
+        skip_leading_rows=1,
+    )
+    uri = "gs://danipipe1/data.csv"
+
+    load_job = client.load_table_from_uri(
+    uri, table_id, job_config=job_config
+    )  # Make an API request.
+
+    load_job.result()  # Wait for the job to complete.
+
+    table = client.get_table(table_id)
